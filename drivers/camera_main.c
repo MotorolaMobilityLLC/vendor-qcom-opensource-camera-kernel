e43eebd368ea8105439ac4a0c633752261589621
huaqin90 <huaqin90@motorola.com>
2021-12-03 11:44:34 +0800

    Rhodep: flash bring up(1/2)
    details:
    flash bring up
    
    Change-Id: Ic076ca9324f3ab0a0e83493484c14529ae3677b3
    Signed-off-by: huaqin90 <huaqin90@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2134124
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Long Cheng <chengl1@motorola.com>
    Reviewed-by: Ying Xu <xuying9@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/camera_main.c
+++ drivers/camera_main.c
@@ -28,6 +28,7 @@
 #include "cam_csiphy_dev.h"
 #include "cam_eeprom_dev.h"
 #include "cam_ois_dev.h"
+#include "pm6125_flash_gpio.h"
 
 #include "cam_flash_dev.h"
 
@@ -118,6 +119,9 @@ static const struct camera_submodule_component camera_sensor[] = {
 	{&cam_eeprom_driver_init, &cam_eeprom_driver_exit},
 	{&cam_ois_driver_init, &cam_ois_driver_exit},
 	{&cam_flash_init_module, &cam_flash_exit_module},
+#endif
+#if IS_REACHABLE(CONFIG_CAMERA_FLASH_PWM)
+    {&pm6125_flash_gpio_init_module, &pm6125_flash_gpio_exit_module},
 #endif
 };
 
