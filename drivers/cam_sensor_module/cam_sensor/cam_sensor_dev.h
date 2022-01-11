eb7a1907c0fc9f8d0f1e1708095f0aa2a8a7775f
wangshuo18 <wangshuo18@lenovo.com>
2022-01-11 21:15:57 +0800

    camera-kernel: li: support IIC Addr Switch method
    -for oc32c4c,we must write 0x1001=0x4 to switch iic addr to 0x20 before
    any iic operation.So add kernel support for it.
    -preInitSetting will be wroten to sensor before write initsetting.
    -IICSwitchRegData will be wroten to sensor before read sensorID during
    sensor probe .
    
    Change-Id: I076bd8818e898f82490448257323e71e350a8620
    Signed-off-by: wangshuo18 <wangshuo18@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2167415
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Xingyun Fu <fuxy7@motorola.com>
    Reviewed-by: Long Cheng <chengl1@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Zhichao Chen <chenzc2@motorola.com>
    Tested-by: Zhichao Chen <chenzc2@motorola.com>

--- drivers/cam_sensor_module/cam_sensor/cam_sensor_dev.h
+++ drivers/cam_sensor_module/cam_sensor/cam_sensor_dev.h
@@ -163,6 +163,14 @@ struct cam_sensor_ctrl_t {
 	uint64_t                       req_table[MAX_PER_FRAME_ARRAY];
 	struct list_head               read_buf_list;
 	struct mutex                   read_buf_lock;
+	/* add i2c addr switch support*/
+	uint8_t                        i2c_addr_switch;
+	uint16_t                       second_i2c_address;
+	uint8_t                        i2c_switch_reg_addr_Type;
+	uint8_t                        i2c_switch_reg_data_Type;
+	uint16_t                       i2c_switch_reg_addr;
+	uint16_t                       i2c_switch_reg_data;
+	uint16_t                       i2c_switch_reg_delayMs;
 };
 
 /**
