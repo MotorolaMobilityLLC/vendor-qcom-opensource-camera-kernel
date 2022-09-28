99d3eba59b0c19621aedd55820bdf99d002a421c
huqian4 <huqian4@motorola.com>
2022-09-28 11:04:54 +0800

    Canyon: ctle modify for RF
    details:
    modify cam_csiphy_2_1_2_hwreg.h
    
    Change-Id: I9044cc085062abcf4b9533c7d9db0d32c9ca41c3
    Signed-off-by: huqian4 <huqian4@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2393082
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Shuo Wang <wangsh18@motorola.com>
    Reviewed-by: <hezheng1@motorola.com>
    Reviewed-by: Wang Liu <liuwang2@lenovo.com>
    Reviewed-by: Long Cheng <chengl1@motorola.com>
    Reviewed-by: Zhuoran Xu <xuzr3@motorola.com>
    Submit-Approved: Jira Key

--- config/rtwo.mk
+++ config/rtwo.mk
@@ -4,7 +4,9 @@
 # MMI_STOPSHIP Camera: Remove for user build
 CONFIG_CCI_DEBUG_INTF := y
 CONFIG_CAM_SENSOR_PROBE_DEBUG := y
+CONFIG_CAM_SENSOR_CTLE_FOR_CANYON := y
 
 # Flags to pass into C preprocessor
 ccflags-y += -DCONFIG_CCI_DEBUG_INTF=1
 ccflags-y += -DCONFIG_CAM_SENSOR_PROBE_DEBUG=1
+ccflags-y += -DCONFIG_CAM_SENSOR_CTLE_FOR_CANYON=1
