211fa6377089147f5aa04c5b2a113d5236e88220
LiuWang <liuwang2@motorola.com>
2023-10-19 14:15:15 +0800

    ctwov: add CONFIG_MOT_OIS_AF_USE_SAME_IC
    [detail]
    add CONFIG_MOT_OIS_AF_USE_SAME_IC for ctwov
    
    Change-Id: I19ec557ea9ab4beb1277fee4ea0be14dced69f41
    Signed-off-by: LiuWang <liuwang2@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2772136
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Wang Liu <liuwang2@lenovo.com>
    Reviewed-by: Ji Zhao <zhaoji1@lenovo.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    SLTApproved: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Zhichao Chen <chenzc2@motorola.com>
    Tested-by: Zhichao Chen <chenzc2@motorola.com>

--- drivers/cam_sensor_module/cam_actuator/cam_actuator_dev.h
+++ drivers/cam_sensor_module/cam_actuator/cam_actuator_dev.h
@@ -120,6 +120,9 @@ struct cam_actuator_ctrl_t {
 	struct cam_actuator_query_cap act_info;
 	struct actuator_intf_params bridge_intf;
 	uint32_t last_flush_req;
+#ifdef CONFIG_MOT_OIS_AF_USE_SAME_IC
+	bool af_ois_use_same_ic;
+#endif
 };
 
 /**
--- drivers/cam_sensor_module/cam_actuator/cam_actuator_soc.c
+++ drivers/cam_sensor_module/cam_actuator/cam_actuator_soc.c
@@ -43,6 +43,15 @@ int32_t cam_actuator_parse_dt(struct cam_actuator_ctrl_t *a_ctrl,
 
 	CAM_DBG(CAM_SENSOR, "I3C Target: %s", CAM_BOOL_TO_YESNO(a_ctrl->is_i3c_device));
 
+#ifdef CONFIG_MOT_OIS_AF_USE_SAME_IC
+	if (!of_property_read_bool(of_node, "af-ois-use-same-ic")) {
+		a_ctrl->af_ois_use_same_ic = false;
+	} else {
+		a_ctrl->af_ois_use_same_ic = true;
+	}
+	CAM_DBG(CAM_ACTUATOR, "af_ois_use_same_ic %d", a_ctrl->af_ois_use_same_ic);
+#endif
+
 	if (a_ctrl->io_master_info.master_type == CCI_MASTER) {
 		rc = of_property_read_u32(of_node, "cci-master",
 			&(a_ctrl->cci_i2c_master));
