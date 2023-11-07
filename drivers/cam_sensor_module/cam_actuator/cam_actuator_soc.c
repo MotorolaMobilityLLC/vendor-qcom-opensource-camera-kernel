11dbefca6b018660a752d8251cb22c240c599a3c
LiuWang <liuwang2@motorola.com>
2023-11-07 15:57:36 +0800

    ctwov: add CONFIG_MOT_DONGWOON_OIS_AF_DRIFT
    [detail]
    CONFIG_MOT_DONGWOON_OIS_AF_DRIFT is used for dw9784 af drift
    
    Change-Id: Ieea2ac522ef994e50068c2ef49a63a3c2f48e29e
    Signed-off-by: LiuWang <liuwang2@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2787984
    Reviewed-by: Wang Liu <liuwang2@lenovo.com>
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: <liuyy62@motorola.com>
    Reviewed-by: XiaoFeng Wang <wangxf32@lenovo.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/cam_sensor_module/cam_actuator/cam_actuator_soc.c
+++ drivers/cam_sensor_module/cam_actuator/cam_actuator_soc.c
@@ -52,6 +52,15 @@ int32_t cam_actuator_parse_dt(struct cam_actuator_ctrl_t *a_ctrl,
 	CAM_INFO(CAM_ACTUATOR, "af_ois_use_same_ic %d", a_ctrl->af_ois_use_same_ic);
 #endif
 
+#ifdef CONFIG_MOT_DONGWOON_OIS_AF_DRIFT
+	if (!of_property_read_bool(of_node, "af-drift-support")) {
+		a_ctrl->af_drift_supported = false;
+	} else {
+		a_ctrl->af_drift_supported = true;
+	}
+	CAM_INFO(CAM_ACTUATOR, "af-drift-support %d", a_ctrl->af_drift_supported);
+#endif
+
 	if (a_ctrl->io_master_info.master_type == CCI_MASTER) {
 		rc = of_property_read_u32(of_node, "cci-master",
 			&(a_ctrl->cci_i2c_master));
