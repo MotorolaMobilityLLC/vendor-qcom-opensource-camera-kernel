3d4e6d04ca016f1ad5eec65a8d64ba2efaaaafd5
hezheng1 <hezheng1@motorola.com>
2024-03-21 15:19:51 +0800

    wrangler:add sensor probe retry in factory mode
    details:
    1.enable CAM_SENSOR_PROBE_DEBUG
    
    Change-Id: I695e4f5e0de46598319d754217e49479cabbddbf
    Signed-off-by: hezheng1 <hezheng1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2913320
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Pengfei Qiao <qiaopf1@motorola.com>
    Reviewed-by: <yangmf3@motorola.com>
    Reviewed-by: <huqian4@motorola.com>
    Reviewed-by: XiaoFeng Wang <wangxf32@lenovo.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- config/tank_factory.mk
+++ config/tank_factory.mk
@@ -2,6 +2,8 @@
 # Localized KCONFIG settings
 CONFIG_CCI_DEBUG_INTF := y
 ccflags-y += -DCONFIG_CCI_DEBUG_INTF=1
+CONFIG_CAM_SENSOR_PROBE_DEBUG := y
+ccflags-y += -DCONFIG_CAM_SENSOR_PROBE_DEBUG=1
 
 # Flags to pass into C preprocessor
 CONFIG_MOT_OIS_DW9784_DRIVER := y
