71ece81aac5890b469f3d4425359dfc9063561d7
PeiFei Ding <am2309@motorola.com>
2024-03-25 19:01:31 +0800

    Malmo: cam_csiphy hw version modify for faceunlock
    details:
    QCOM case:07108330
    cam_csiphy hw version modify by qcom
    
    Change-Id: Ia668157c14f497f87cf1a393f2164066e798440d
    Reviewed-on: https://gerrit.mot.com/2917264
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Zilong Zhang <zhangzl7@lenovo.com>
    Reviewed-by: Shanghui Zhang <zhangsh@motorola.com>
    Reviewed-by: Zhuoran Xu <xuzr3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/cam_sensor_module/cam_csiphy/cam_csiphy_core.c
+++ drivers/cam_sensor_module/cam_csiphy/cam_csiphy_core.c
@@ -207,11 +207,11 @@ static int32_t cam_csiphy_update_secure_info(
 	switch (csiphy_dev->hw_version) {
 	case CSIPHY_VERSION_V201:
 	case CSIPHY_VERSION_V125:
+	case CSIPHY_VERSION_V121:
 	case CSIPHY_VERSION_V123:
 		phy_mask_len =
 		CAM_CSIPHY_MAX_DPHY_LANES + CAM_CSIPHY_MAX_CPHY_LANES + 1;
 		break;
-	case CSIPHY_VERSION_V121:
 	case CSIPHY_VERSION_V124:
 		phy_mask_len =
 		(csiphy_dev->soc_info.index < MAX_PHY_MSK_PER_REG) ?
