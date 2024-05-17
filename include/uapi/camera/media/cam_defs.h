08f620881f0861c233fec349b7c50c6fefe4e803
Lokesh Kumar Aakulu <quic_lkumar@quicinc.com>
2024-05-18 00:28:01 +0530

    msm: camera: uapi: Adding flex array support for ubwc blob
    This change adds flex arrays support for UBWC plane config
    structures and avoid UBSAN errors.
    
    CRs-Fixed: 3821125
    Change-Id: I3f663bcc8a2ff839260ac09030bb218ea5ad84e7
    Signed-off-by: Lokesh Kumar Aakulu <quic_lkumar@quicinc.com>
    Signed-off-by: Pritam Sarkar <quic_pritsark@quicinc.com>

--- include/uapi/camera/media/cam_defs.h
+++ include/uapi/camera/media/cam_defs.h
@@ -730,8 +730,15 @@ struct cam_flush_dev_cmd {
 struct cam_ubwc_config {
 	__u32   api_version;
 	__u32   num_ports;
-	struct cam_ubwc_plane_cfg_v1
-		   ubwc_plane_cfg[1][CAM_PACKET_MAX_PLANES - 1];
+	union {
+		struct cam_ubwc_plane_cfg_v1
+			ubwc_plane_cfg[1][CAM_PACKET_MAX_PLANES - 1];
+		struct {
+			struct { } __empty_ubwc_plane_cfg_array_flex;
+			struct cam_ubwc_plane_cfg_v1
+				ubwc_plane_cfg_array_flex[][CAM_PACKET_MAX_PLANES - 1];
+		};
+	};
 };
 
 /**
@@ -748,8 +755,15 @@ struct cam_ubwc_config {
 struct cam_ubwc_config_v2 {
 	__u32   api_version;
 	__u32   num_ports;
-	struct cam_ubwc_plane_cfg_v2
-	   ubwc_plane_cfg[1][CAM_PACKET_MAX_PLANES - 1];
+	union {
+		struct cam_ubwc_plane_cfg_v2
+			ubwc_plane_cfg[1][CAM_PACKET_MAX_PLANES - 1];
+		struct {
+			struct { } __empty_ubwc_plane_cfg_array_flex;
+			struct cam_ubwc_plane_cfg_v2
+				ubwc_plane_cfg_array_flex[][CAM_PACKET_MAX_PLANES - 1];
+		};
+	};
 };
 
 /**
