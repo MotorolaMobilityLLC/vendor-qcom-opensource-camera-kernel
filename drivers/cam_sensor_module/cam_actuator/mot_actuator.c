ce50ee156f8d32ffe8b33aed2d37621d26b510ce
wangxiaofeng <wangxf32@motorola.com>
2024-09-09 14:43:34 +0800

    leap: Enable lens protection feature
    Details:
    1.Code porting from 8635.
    2.Add AK7316 driver info.
    
    Change-Id: I19ad36a8391b2c475ff6fbc011d7766839d85602
    Signed-off-by: wangxiaofeng <wangxf32@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3080944
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: <liuzy55@motorola.com>
    Reviewed-by: wangdp11 <wangdp11@motorola.com>
    Reviewed-by: Suna Li <lisn17@motorola.com>
    Reviewed-by: Liu Wang <liuwang2@lenovo.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/cam_sensor_module/cam_actuator/mot_actuator.c
+++ drivers/cam_sensor_module/cam_actuator/mot_actuator.c
@@ -56,6 +56,7 @@ typedef enum {
 
 typedef enum {
 	MOT_DEVICE_CTWO,
+	MOT_DEVICE_LEAP,
 	MOT_DEVICE_NUM,
 } mot_dev_type;
 
@@ -207,6 +208,25 @@ static const mot_dev_info mot_dev_list[MOT_DEVICE_NUM] = {
 			},
 		},
 	},
+	{
+		.dev_type = MOT_DEVICE_LEAP,
+		.actuator_num = 1,
+		.dev_name = "leap",
+		.actuator_info = {
+			[0] = {
+				.actuator_type = MOT_ACTUATOR_AK7316,
+				.dac_pos = 0x2000, //register 01h bit2-bit7 valid
+				.cci_addr = 0x0c,
+				.cci_dev = 0x00,
+				.cci_master = 0x00,
+				.has_ois = false, //main camera use SOIS
+				.regulator_list = {"pm8010m_l4", "pm8010m_l5"}, //VAF VIO
+				.regulator_volt_uv = {2800000, 1800000},
+				.park_lens_needed = false,
+				.poweron_delay = 6,
+			},
+		},
+	},
 };
 
 
@@ -339,7 +359,6 @@ static mot_reset_lens_step lens_reset_table[RESET_LENS_MAX_STAGES] = {
 };
 
 static bool runtime_inited = 0;
-static bool mot_actuator_driver_probed = false;
 
 static int mot_actuator_init_runtime(void)
 {
@@ -1364,34 +1383,39 @@ static void mot_actuator_platform_shutdown(struct platform_device *pdev)
 	unsigned int consumers = 0;
 	int actuatorUsers = 0;
 	ktime_t start,end,duration;
-	if (mot_actuator_driver_probed == true) {
-		mutex_lock(&mot_actuator_fctrl.actuator_lock);
-		consumers = mot_actuator_get_consumers();
-		/*Only reset lens when vibrator in consumer list*/
-		if ((consumers & CLINET_VIBRATOR_MASK) != 0) {
-			actuatorUsers = mot_actuator_put(ACTUATOR_CLIENT_VIBRATOR);
-			start = ktime_get();
-			if (mot_actuator_state >= MOT_ACTUATOR_INITED && mot_actuator_state < MOT_ACTUATOR_RELEASED) {
-				mot_actuator_power_off(0);
-				mot_actuator_release_cci(0);
-				if (mot_dev_list[mot_device_index].actuator_info[0].has_ois) {
-					mot_ois_stop_protection(&mot_actuator_fctrl.v4l2_dev_str.pdev->dev);
-				}
-			}
-			mot_actuator_state = MOT_ACTUATOR_RELEASED;
-			end = ktime_get();
-			duration = ktime_sub(end, start);
-			duration /= 1000;
-			CAM_WARN(CAM_ACTUATOR, "reset lens delay: %dus", duration);
-		}
-		mot_actuator_uninit_runtime();
-		if (mot_dev_list[mot_device_index].actuator_info[0].has_ois) {
-			mot_ois_handle_shut_down(&mot_actuator_fctrl.v4l2_dev_str.pdev->dev);
-		}
-		mutex_unlock(&mot_actuator_fctrl.actuator_lock);
+	/* cam actuator or mot actuator probe function not executed cause all camera module register failed,
+	let mot_device_index is default value MOT_DEVICE_NUM, mot_dev_list = NULL cause kernel panic. */
+	if ((mot_device_index >= MOT_DEVICE_NUM) || (ARRAY_SIZE(mot_dev_list) == 0)) {
+		CAM_ERR(CAM_ACTUATOR, "mot actuator shutdown mot_device_index = %d", mot_device_index);
+		return;
 	}
 
-	mot_actuator_driver_probed = false;
+	mutex_lock(&mot_actuator_fctrl.actuator_lock);
+	consumers = mot_actuator_get_consumers();
+	/*Only reset lens when vibrator in consumer list*/
+	if ((consumers & CLINET_VIBRATOR_MASK) != 0) {
+		actuatorUsers = mot_actuator_put(ACTUATOR_CLIENT_VIBRATOR);
+		start = ktime_get();
+		if (mot_actuator_state >= MOT_ACTUATOR_INITED && mot_actuator_state < MOT_ACTUATOR_RELEASED) {
+			mot_actuator_power_off(0);
+			mot_actuator_release_cci(0);
+			if (mot_dev_list[mot_device_index].actuator_info[0].has_ois) {
+				mot_ois_stop_protection(&mot_actuator_fctrl.v4l2_dev_str.pdev->dev);
+			}
+		}
+		mot_actuator_state = MOT_ACTUATOR_RELEASED;
+		end = ktime_get();
+		duration = ktime_sub(end, start);
+		duration /= 1000;
+		CAM_WARN(CAM_ACTUATOR, "reset lens delay: %dus", duration);
+	}
+	mot_actuator_uninit_runtime();
+	if (mot_dev_list[mot_device_index].actuator_info[0].has_ois) {
+		mot_ois_handle_shut_down(&mot_actuator_fctrl.v4l2_dev_str.pdev->dev);
+	}
+
+	mutex_unlock(&mot_actuator_fctrl.actuator_lock);
+
 	return;
 }
 
@@ -1407,12 +1431,8 @@ static int32_t mot_actuator_driver_platform_probe(
 
 	CAM_DBG(CAM_ACTUATOR, "Adding sensor actuator component");
 	rc = component_add(&pdev->dev, &mot_actuator_component_ops);
-	if (rc) {
-		mot_actuator_driver_probed = false;
+	if (rc)
 		CAM_ERR(CAM_ICP, "failed to add component rc: %d", rc);
-	} else {
-		mot_actuator_driver_probed = true;
-	}
 
 	return rc;
 }
