88b5f8f3b0d500c28606cd9c80e4993f200a0c32
chenlj26 <chenlj26@motorola.com>
2024-09-30 18:27:21 +0800

    mona: modify the failed of ANDROID_TELL_CAMERA2_TAKE_PICTURE_REAR_MAIN_POS_10
    details:
    create a custom file of cam_csiphy_1_2_3_hwreg_mona.h
    define CONFIG_MOT_SM6475_MONA for mona
    define CONFIG_MOT_CUSTOM_CTLE_PARAM for mona
    test result: pass
    rely: na
    
    Change-Id: I4f4ffdcb949b481e3842e346e53d4a52bd688eca
    Signed-off-by: chenlj26 <chenlj26@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3100664
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Tested-by: Pdo Scm <hudsoncm@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Reviewed-by: <huqian4@motorola.com>
    Reviewed-by: Pengfei Qiao <qiaopf1@motorola.com>
    Submit-Approved: Jira Key

--- config/mona_factory.mk
+++ config/mona_factory.mk
@@ -8,5 +8,7 @@ ccflags-y += -DCONFIG_CCI_DEBUG_INTF=1
 endif
 CONFIG_CAM_SENSOR_PROBE_DEBUG := y
 
+ccflags-y += -DCONFIG_MOT_CUSTOM_CTLE_PARAM=1
+ccflags-y += -DCONFIG_MOT_SM6475_MONA=1
 # Flags to pass into C preprocessor
 ccflags-y += -DCONFIG_CAM_SENSOR_PROBE_DEBUG=1
