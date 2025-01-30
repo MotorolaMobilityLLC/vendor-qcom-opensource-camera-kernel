27ec24b415c71da4f5ca61bd3d93bed26af94d6d
Mukund Madhusudan Atre <quic_matre@quicinc.com>
2025-01-30 11:51:07 -0800

    msm: camera: isp: Vote drv low value for core clock always
    Currently, the low value for core clocks of RT cores is voted
    only if DRV config is enabled. Change this logic so that low
    value is voted if the hw supports it.
    
    CRs-Fixed: 3800048
    Change-Id: Iff02769125b8dd2544dd9d7b26cfff216a3abc58
    Signed-off-by: Mukund Madhusudan Atre <quic_matre@quicinc.com>

--- drivers/cam_isp/isp_hw_mgr/isp_hw/sfe_hw/sfe_top/cam_sfe_top.c
+++ drivers/cam_isp/isp_hw_mgr/isp_hw/sfe_hw/sfe_top/cam_sfe_top.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2020-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2025 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <linux/slab.h>
@@ -192,11 +192,7 @@ static int cam_sfe_top_set_hw_clk_rate(struct cam_sfe_top_priv *top_priv,
 
 	if (soc_info->is_clk_drv_en) {
 		cesta_client_idx = top_priv->common_data.hw_intf->hw_idx;
-		if (is_drv_config_en)
-			cesta_clk_rate_low =
-				soc_info->clk_rate[lowest_clk_lvl][soc_info->src_clk_idx];
-		else
-			cesta_clk_rate_low = final_clk_rate;
+		cesta_clk_rate_low = soc_info->clk_rate[lowest_clk_lvl][soc_info->src_clk_idx];
 	}
 
 	CAM_DBG(CAM_PERF|CAM_ISP,
