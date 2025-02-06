061870b640da934233e191b97b43e4ff677acad2
xuc.li <xuc.li@archermind.com>
2025-02-06 10:56:48 +0800

    Qcom patch(2/2)
    Details:Fix the viewfinder flashing when manual adjust the exposure to minimu and change the zoom ratio in slow motion mode.
    
    Case num:7599385
    
    Edart-CR: EKMALMO-2424
    Reviewed-on: https://gerrit.mot.com/3266464
    Reviewed-on: https://gerrit-odm.mot.com/c/home/repo/dev/platform/android/platform/vendor/opensource/camera-kernel/+/16707
    Change-Id: I500c67604381b7b1995bd494fed054c9bfca4d7f
    Reviewed-on: https://gerrit-odm.mot.com/c/home/repo/dev/platform/android/platform/vendor/opensource/camera-kernel/+/16707
    Tested-by: gerrito
    Reviewed-by: Litong Wei <weilt2@motorola.com>
    Reviewed-by: Lei Wang <WANGL67@motorola.com>
    Reviewed-by: Zhuoran Xu <xuzr3@motorola.com>

--- drivers/cam_sensor_module/cam_sensor/cam_sensor_dev.h
+++ drivers/cam_sensor_module/cam_sensor/cam_sensor_dev.h
@@ -63,11 +63,27 @@ struct sensor_intf_params {
 };
 
 /**
+ * struct cam_sensor_dev_res_info
+ *
+ * @res_index        : The resolution index that gets updated
+ *                     during a mode switch
+ * @feature_mask     : Feature mask
+ * request_id        : Request Id
+ */
+struct cam_sensor_dev_res_info {
+	uint16_t   res_index;
+	uint32_t   num_batched_frames;
+	uint16_t   feature_mask;
+	int64_t    request_id;
+};
+
+/**
  * struct cam_sensor_ctrl_t: Camera control structure
  * @device_name: Sensor device name
  * @pdev: Platform device
  * @cam_sensor_mutex: Sensor mutex
  * @sensordata: Sensor board Information
+ * @sensor_res: Sensor resolution information
  * @cci_i2c_master: I2C structure
  * @io_master_info: Information about the communication master
  * @sensor_state: Sensor states
@@ -86,6 +102,7 @@ struct sensor_intf_params {
  * @bob_pwm_switch: Boolean flag to switch into PWM mode for BoB regulator
  * @last_flush_req: Last request to flush
  * @pipeline_delay: Sensor pipeline delay
+ * @batch_number: Number of batched frames
  */
 struct cam_sensor_ctrl_t {
 	char device_name[CAM_CTX_DEV_NAME_MAX_LENGTH];
@@ -93,6 +110,7 @@ struct cam_sensor_ctrl_t {
 	struct cam_hw_soc_info soc_info;
 	struct mutex cam_sensor_mutex;
 	struct cam_sensor_board_info *sensordata;
+	struct cam_sensor_dev_res_info sensor_res;
 	enum cci_i2c_master_t cci_i2c_master;
 	enum cci_device_num cci_num;
 	struct camera_io_master io_master_info;
@@ -112,6 +130,7 @@ struct cam_sensor_ctrl_t {
 	bool bob_pwm_switch;
 	uint32_t last_flush_req;
 	uint16_t pipeline_delay;
+	uint32_t batch_number;
 	uint32_t probe_sensor_slave_addr;
 	uint8_t   probe_sub_device;
 	uint16_t  sub_device_addr;
--- drivers/cam_sensor_module/cam_sensor_utils/cam_sensor_cmn_header.h
+++ drivers/cam_sensor_module/cam_sensor_utils/cam_sensor_cmn_header.h
@@ -33,6 +33,12 @@
 
 #define CAM_PKT_NOP_OPCODE 127
 
+/* Sensor Driver cmd buffer meta type */
+#define CAM_SENSOR_PACKET_GENERIC_BLOB             1
+
+/* Sensor Res Blob Type */
+#define CAM_SENSOR_GENERIC_BLOB_RES_INFO           0
+
 enum camera_sensor_cmd_type {
 	CAMERA_SENSOR_CMD_TYPE_INVALID,
 	CAMERA_SENSOR_CMD_TYPE_PROBE,
@@ -158,6 +164,7 @@ enum cam_sensor_packet_opcodes {
 	CAM_SENSOR_PACKET_OPCODE_SENSOR_FRAME_SKIP_UPDATE,
 	CAM_SENSOR_PACKET_OPCODE_SENSOR_REG_BANK_UNLOCK,
 	CAM_SENSOR_PACKET_OPCODE_SENSOR_REG_BANK_LOCK,
+	CAM_SENSOR_PACKET_OPCODE_SENSOR_RESCONFIG = 126,
 	CAM_SENSOR_PACKET_OPCODE_SENSOR_NOP = 127
 };
 
@@ -423,5 +430,28 @@ struct msm_camera_gpio_conf {
 	uint8_t camera_on_table_size;
 	struct msm_camera_gpio_num_info *gpio_num_info;
 };
+
+/**
+ * struct cam_cmd_sensor_res_info - Contains sensor res info
+ *
+ * res_index is the key property, it specifies the
+ * combinations of other properties enclosed in this
+ * structure.
+ *
+ * @version           :Version to indicate the change
+ * @res_index         : Sensor resolution index
+ * @num_batched_frames: Number of batched frames
+ * @num_valid_params  : Number of valid params
+ * @valid_param_mask  : Valid param mask
+ * @params            : params
+ */
+struct cam_sensor_res_info {
+	__u32 version;
+	__u16 res_index;
+	__u16 num_batched_frames;
+	__u32 num_valid_params;
+	__u32 valid_param_mask;
+	__u16 params[4];
+} __attribute__((packed));
 
 #endif /* _CAM_SENSOR_CMN_HEADER_ */
