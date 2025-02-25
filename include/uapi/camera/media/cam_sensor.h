45f918b1bda179a18cd9dd577db0484acbd5d599
Simon C <chenzm8@motorola.com>
2025-02-25 14:07:08 +0800

    fogona: Compatible solution for 2nd main camera module - kernel
    Details:
      -Match the subdevice id to distinguish 1st and 2nd modules.
    
    Change-Id: I2b4d135f59be9d5a141f9c11c674bcb089148fa1
    Signed-off-by: Simon C <chenzm8@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3266829
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: wangxf32 <wangxf32@lenovo.com>
    Reviewed-by: Shuo Wang <wangsh18@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- include/uapi/camera/media/cam_sensor.h
+++ include/uapi/camera/media/cam_sensor.h
@@ -166,6 +166,12 @@ struct cam_cmd_probe {
 	uint32_t    data_mask;
 	uint16_t    camera_id;
 	uint16_t    reserved;
+	uint8_t     probe_sub_device;
+	uint32_t    sub_device_addr;
+	uint8_t     sub_device_data_type;
+	uint8_t     sub_device_addr_type;
+	uint32_t    sub_device_id_addr;
+	uint32_t    expected_sub_device_id;
 } __attribute__((packed));
 
 /**
