212c6514440936562bdcad92d0aff405ff04a6f8
Abhilash Kumar <quic_krabhi@quicinc.com>
2025-04-10 11:51:14 -0700

    msm: camera: isp: Print HBI and VBI in case of CSID error
    This change adds logic to print HBI and VBI in case CSID error
    is encountered.
    
    CRs-Fixed: 4082438
    Change-Id: I7a8896414a19ab3a4c452819922cabd82bd89e0b
    Signed-off-by: Abhilash Kumar <quic_krabhi@quicinc.com>

--- drivers/cam_isp/isp_hw_mgr/isp_hw/ife_csid_hw/cam_ife_csid_hw_ver2.c
+++ drivers/cam_isp/isp_hw_mgr/isp_hw/ife_csid_hw/cam_ife_csid_hw_ver2.c
@@ -2054,6 +2054,8 @@ static int cam_ife_csid_ver2_handle_event_err(
 	evt.in_core_idx =
 		cam_ife_csid_ver2_input_core_to_hw_idx(csid_hw->top_cfg.input_core_type);
 
+	cam_ife_csid_ver2_print_hbi_vbi(csid_hw);
+
 	cam_ife_csid_ver2_print_camif_timestamps(csid_hw);
 
 	cam_ife_csid_ver2_read_debug_err_vectors(csid_hw);
@@ -8160,7 +8162,7 @@ static int cam_ife_csid_ver2_print_hbi_vbi(
 			path_reg->base + path_data->reg_offsets->format_measure1_addr);
 		vbi = cam_io_r_mb(soc_info->reg_map[0].mem_base +
 			path_reg->base + path_data->reg_offsets->format_measure2_addr);
-		CAM_INFO_RATE_LIMIT(CAM_ISP,
+		CAM_INFO(CAM_ISP,
 			"CSID[%u] Resource[id:%d, name:%s, min_hbi: %d max_hbi: %d cycles, vbi: %d cycles]",
 			csid_hw->hw_intf->hw_idx, res->res_id, res->res_name,
 			(hbi & cmn_reg->format_measure_min_hbi_mask),
