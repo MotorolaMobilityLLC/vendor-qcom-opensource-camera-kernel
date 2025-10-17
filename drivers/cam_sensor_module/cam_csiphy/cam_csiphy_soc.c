67f929ff9f3195d4945ca5065ccbedde3a24a004
ZHENGB8 <zhengb8@motorola.com>
2025-10-17 15:10:26 +0800

    lamy: drv: modify the failed of ANDROID_TELL_CAMERA2_FACTORY_TAKE_PICTURE_REAR_MAIN_POS TEST FAIL
    details:
    1. create a custom file of cam_csiphy_1_2_3_hwreg_mona.h
    2. define CONFIG_MOT_SM6475_LAMY for lamy
    3. open CONFIG_MOT_CUSTOM_CTLE_PARAM for lamy
    
    Change-Id: I8711ed9d2e42246c3496137f0ebe326547455ba1
    Signed-off-by: ZHENGB8 <zhengb8@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3596240
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Reviewed-by: Pdo Scm <hudsoncm@motorola.com>
    Tested-by: Jira Key
    Reviewed-by: <huqian4@motorola.com>
    Reviewed-by: <guojx7@motorola.com>
    Reviewed-by: <wuql2@motorola.com>
    Reviewed-by: <shenzm2@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/cam_sensor_module/cam_csiphy/cam_csiphy_soc.c
+++ drivers/cam_sensor_module/cam_csiphy/cam_csiphy_soc.c
@@ -24,6 +24,9 @@
 #ifdef CONFIG_MOT_SM6475_PORTOV
 #include "include/cam_csiphy_1_2_3_hwreg_portov.h"
 #endif
+#ifdef CONFIG_MOT_SM6475_LAMY
+#include "include/cam_csiphy_1_2_3_hwreg_lamy.h"
+#endif
 #endif
 #include "include/cam_csiphy_1_2_5_hwreg.h"
 #include "include/cam_csiphy_2_0_hwreg.h"
@@ -426,7 +429,7 @@ int32_t cam_csiphy_parse_dt_info(struct platform_device *pdev,
 		csiphy_dev->clk_lane = 0;
 		csiphy_dev->ctrl_reg->data_rates_settings_table = &data_rate_delta_table_1_2_3;
 #ifdef CONFIG_MOT_CUSTOM_CTLE_PARAM
-#if defined(CONFIG_MOT_SM7435_AVATRN) || defined(CONFIG_MOT_SM6450_CUSCO) || defined(CONFIG_MOT_SM6475_MONA)
+#if defined(CONFIG_MOT_SM7435_AVATRN) || defined(CONFIG_MOT_SM6450_CUSCO) || defined(CONFIG_MOT_SM6475_MONA) || defined(CONFIG_MOT_SM6475_LAMY)
 		if (soc_info->index == 1) {
 			csiphy_dev->ctrl_reg->csiphy_3ph_reg = csiphy_3ph_v1_2_3_reg_custom;
 			csiphy_dev->ctrl_reg->data_rates_settings_table = &data_rate_delta_table_1_2_3_custom;
